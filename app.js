e6633930199056420337c0bb271db452ec7d6cdb
mbytemak <34517148+mbytemak@users.noreply.github.com>
2018-03-21 12:05:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 
+ 
   $( document ).ready(function() {
 
     $(".items").hide();
       $(".status").hide();
 
 
-    var myFacebookToken = 'EAACEdEose0cBAGSmyzv4fcUAfjNx8lY0ONk3NaPs1XkPLQulVwLXJ44q16ZBykK8SsYlqxp9ldi9SN3MoUrkKxJZA8FVNoI2I3p0wn9xKy3FI19Ocw3dSWFxV4Dd5x8v2ZAKSmd8X7sx9mi8tZAxlZAo0lDL4E5qAzYU2DZA9zybtFUVCBykoIREBRBj6zZC7IZD';
+    var myFacebookToken = 'EAACEdEose0cBAEwYKXtnQd0VxXXDfL05qoeunBt6cTn7K5YJHLhywF08xqH8raPOu2mizZACy30ZCaPVjYYTul0snjGmp9BnRxCNbyU1nNWnJItESCZAi6ZBbsSPWTUVXjzi2jbW4ohZBOqmvriEyaCUW76GmmPtGjZBdYZAqER7FD2dsEmV8U99WPEQ0VkNVDQf4ekooY9GgZDZD';
 
     function getFacebookInfo(){
 
         $.ajax(
- 'https://graph.facebook.com/v2.12/me?fields=name%2Cbirthday%2Chometown%2Clanguages%2Cposts&access_token='+myFacebookToken,{
+ 'https://graph.facebook.com/me?fields=picture.width(250).height(250),id,name,first_name,last_name,birthday,about,hometown,languages,gender,education,work,relationship_status,quotes,family,website,email,cover.width(815).height(320)&access_token='+myFacebookToken,{
 
                 success : function(response){
                     console.log(response);
                     console.log(typeof(response));
                     $("#name").text(response.name);
+if($("#picture img").length == 0){
+                    $('#picture').append('<img src="'+response.picture.data.url+'">');
+}
+
+
+
 for( var i=0;i<response.languages.length;i++){
-
-
-
-if(i<=response.languages.length){
 $("#lang").append(response.languages[i].name +",");
 
-}}
+}
 
 
 
                $("#birthday").text(response.birthday);
                     $("#myHomeTown").text(response.hometown.name);
-
+          $("#email").text(response.email);
 
                 },
                 error : function(request,errorType,errorMessage){
@@ -62,15 +65,17 @@ $(".status").hide();
     function getFacebookStatusInfo(){
 
         $.ajax(
-  'https://graph.facebook.com/v2.12/me?fields=name%2Cbirthday%2Chometown%2Clanguages%2Cposts&access_token='+myFacebookToken,{
+  'https://graph.facebook.com/me?fields=posts{created_time,type,full_picture,story,message,source},picture.width(250).height(250),cover,likes&access_token='+myFacebookToken
+,{
 
                 success : function(response){
                     console.log(response);
                     console.log(typeof(response));
 
                     for( var i=0;i<response.posts.data.length;i++){
-                        $("#story").append(response.posts.data[i].story +"<br> "+response.posts.data[i].created_time+"<br><br>");
+                        $("#story").append(response.posts.data[i].story +"<br> "+response.posts.data[i].created_time+'<li><img src="'+response.posts.data[i].full_picture+'" height="50%" width="200%" padding:0px 25px 0px 20px></li>'+"<br><br>");
                     }
+
                   },
   error : function(request,errorType,errorMessage){
 
